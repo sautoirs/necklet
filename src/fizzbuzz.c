ad311175d44c4d784f6e3336ce246b020144f7b7
sautoirs <sautoirs@protonmail.com>
2020-07-28 21:55:08 +0200

    feat: add new requirements to fizzbuzz
    Returns Fizz if the number has 3 in it and returns
    Buzz if the number has 5 in it.

--- src/fizzbuzz.c
+++ src/fizzbuzz.c
@@ -1,19 +1,39 @@
 #include <necklet/fizzbuzz.h>
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+
+bool is_fizz(int n);
+bool is_buzz(int n);
 
 size_t fizzbuzz(int n, char *buffer, size_t size)
 {
     size_t written = 0;
-    if ((n % 3 == 0) && (n % 5 == 0)) {
+    if (is_fizz(n) && is_buzz(n)) {
 	written = snprintf(buffer, size, "FizzBuzz");
-    } else if (n % 3 == 0) {
+    } else if (is_fizz(n)) {
 	written = snprintf(buffer, size, "Fizz");
-    } else if (n % 5 == 0) {
+    } else if (is_buzz(n)) {
 	written = snprintf(buffer, size, "Buzz");
     } else {
         written = snprintf(buffer, size, "%d", n);
     }
     return written;
+}
+
+bool is_fizz(int n)
+{
+    char buffer[16];
+    snprintf(buffer, sizeof(buffer) * sizeof(buffer[0]), "%d", n);
+    return ((n % 3) == 0) || (strchr(buffer, '3') != NULL);
+}
+
+bool is_buzz(int n)
+{
+    char buffer[16];
+    snprintf(buffer, sizeof(buffer) * sizeof(buffer[0]), "%d", n);
+    return ((n % 5) == 0) || (strchr(buffer, '5') != NULL);
+
 }
 
--- tests/unit/fizzbuzz.cpp
+++ tests/unit/fizzbuzz.cpp
@@ -11,33 +11,89 @@ SCENARIO("getting fizz, buzz or fizzbuz", "[fizzbuzz]") {
                 REQUIRE(std::string(buffer) == "1");
 	    }
 	}
+
         WHEN("the number is 2") {
             THEN("the function output is 2") {
 		fizzbuzz(2, buffer, sizeof(buffer) * sizeof(buffer[0]));
                 REQUIRE(std::string(buffer) == "2");
 	    }
 	}
+
         WHEN("the number is 3") {
             THEN("the function output is Fizz") {
 		fizzbuzz(3, buffer, sizeof(buffer) * sizeof(buffer[0]));
                 REQUIRE(std::string(buffer) == "Fizz");
 	    }
 	}
+
         WHEN("the number is 4") {
             THEN("the function output is 4") {
 		fizzbuzz(4, buffer, sizeof(buffer) * sizeof(buffer[0]));
                 REQUIRE(std::string(buffer) == "4");
 	    }
 	}
+
         WHEN("the number is 5") {
             THEN("the function output is Buzz") {
 		fizzbuzz(5, buffer, sizeof(buffer) * sizeof(buffer[0]));
                 REQUIRE(std::string(buffer) == "Buzz");
 	    }
 	}
+
         WHEN("the number is 15") {
             THEN("the function output is FizzBuzz") {
 		fizzbuzz(15, buffer, sizeof(buffer) * sizeof(buffer[0]));
+                REQUIRE(std::string(buffer) == "FizzBuzz");
+	    }
+	}
+    }
+
+    GIVEN("a number with 3 in it") {
+	char buffer[32];
+	WHEN("the number is 3") {
+	    THEN("the function output is Fizz") {
+		fizzbuzz(3, buffer, sizeof(buffer) * sizeof(buffer[0]));
+                REQUIRE(std::string(buffer) == "Fizz");
+	    }
+	}
+
+	WHEN("the number is 13") {
+	    THEN("the function output is Fizz") {
+		fizzbuzz(13, buffer, sizeof(buffer) * sizeof(buffer[0]));
+                REQUIRE(std::string(buffer) == "Fizz");
+	    }
+	}
+    }
+
+    GIVEN("a number with 5 in it") {
+	char buffer[32];
+	WHEN("the number is 5") {
+	    THEN("the function output is Buzz") {
+		fizzbuzz(5, buffer, sizeof(buffer) * sizeof(buffer[0]));
+                REQUIRE(std::string(buffer) == "Buzz");
+	    }
+	}
+
+	WHEN("the number is 25") {
+	    THEN("the function output is Buzz") {
+		fizzbuzz(25, buffer, sizeof(buffer) * sizeof(buffer[0]));
+                REQUIRE(std::string(buffer) == "Buzz");
+	    }
+	}
+    }
+
+    GIVEN("a number with both 3 and 5 in it") {
+	char buffer[32];
+	WHEN("the number is 35") {
+	    THEN("the function output is FizzBuzz") {
+		fizzbuzz(35, buffer, sizeof(buffer) * sizeof(buffer[0]));
+                REQUIRE(std::string(buffer) == "FizzBuzz");
+	    }
+	}
+
+	WHEN("the number is 5531") {
+	    THEN("the function output is FizzBuzz") {
+		fizzbuzz(5531, buffer, sizeof(buffer) * sizeof(buffer[0]));
                 REQUIRE(std::string(buffer) == "FizzBuzz");
 	    }
 	}
