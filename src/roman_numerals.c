a61a423f275ecf6aa45e651d867580c51093e929
sautoirs <sautoirs@protonmail.com>
2020-08-14 00:00:32 +0200

    refactor: proceed to translation arabic digit by arabic digit

--- src/roman_numerals.c
+++ src/roman_numerals.c
@@ -1,23 +1,26 @@
 #include <necklet/roman_numerals.h>
 
+#include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
 struct number {
     uint32_t arabic;
-    char roman;
+    char roman[5];
 };
 
-const struct number *get_closest_basic(uint32_t arabic);
-bool in_range(const struct number *self, uint32_t arabic);
+const struct number get_closest_digit(uint32_t arabic);
+void generate_basic_combination(char *output, uint32_t unit, size_t i);
 
 const struct number BASICS[] = {
-    {1, 'I'},
-    {5, 'V'},
-    {10, 'X'},
-    {50, 'L'},
-    {100, 'C'},
-    {500, 'D'},
-    {1000, 'M'},
+    {1, "I"},
+    {5, "V"},
+    {10, "X"},
+    {50, "L"},
+    {100, "C"},
+    {500, "D"},
+    {1000, "M"},
 };
 
 size_t to_roman(char *output, size_t size, uint32_t arabic)
@@ -25,31 +28,63 @@ size_t to_roman(char *output, size_t size, uint32_t arabic)
     size_t written = 0;
     int32_t remaining = arabic;
     do {
-        const struct number *basic = get_closest_basic(remaining);
-        output[written] = basic->roman;
-        written += 1;
-        remaining -= basic->arabic;
+        const struct number digit = get_closest_digit(remaining);
+        strcpy(&output[written], digit.roman);
+        written += strlen(digit.roman);
+        remaining -= digit.arabic;
     } while (remaining > 0);
     return written;
 }
 
-const struct number *get_closest_basic(uint32_t arabic)
+const struct number get_closest_digit(uint32_t arabic)
 {
-    for (size_t i = sizeof(BASICS) / sizeof(BASICS[0]); i >= 0; i--) {
-        const struct number *basic = &BASICS[i];
-        if (in_range(basic, arabic)) {
-            return basic;
+    struct number number = {0, ""};
+    size_t basic_size = sizeof(BASICS) / sizeof(BASICS[0]);
+    for (size_t i = 0; i < basic_size; i += 2) {
+        size_t ri = (((basic_size - 1) / 2) * 2) - i;
+        uint32_t power = pow(10, ri / 2);
+        uint32_t unit = arabic / power;
+        if (unit > 0) {
+            generate_basic_combination(number.roman, unit, ri);
+            number.arabic = unit * power;
+            return number;
         }
     }
-    return NULL;
+    return number;
 }
 
-bool in_range(const struct number *self, uint32_t arabic)
+void generate_basic_combination(char *output, uint32_t unit, size_t i)
 {
-    if (self->arabic <= arabic) {
-        return true;
-    } else {
-        return false;
+    char one = BASICS[i].roman[0];
+    char five = BASICS[i + 1].roman[0];
+    char ten = BASICS[i + 2].roman[0];
+    switch (unit) {
+        case 1:
+            sprintf(output, "%c", one);
+            break;
+        case 2:
+            sprintf(output, "%c%c", one, one);
+            break;
+        case 3:
+            sprintf(output, "%c%c%c", one, one, one);
+            break;
+        case 4:
+            sprintf(output, "%c%c", one, five);
+            break;
+        case 5:
+            sprintf(output, "%c", five);
+            break;
+        case 6:
+            sprintf(output, "%c%c", five, one);
+            break;
+        case 7:
+            sprintf(output, "%c%c%c", five, one, one);
+            break;
+        case 8:
+            sprintf(output, "%c%c%c%c", five, one, one, one);
+            break;
+        case 9:
+            sprintf(output, "%c%c", one, ten);
+            break;
     }
 }
-
